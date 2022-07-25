29839c8171e06ead65a4177ce96a10b5b7326b80
KASHIF INAYAT <26200848+KashifInayat@users.noreply.github.com>
2022-07-26 02:40:50 +0900

    Update libtodb.py

--- libtodb.py
+++ libtodb.py
@@ -3,9 +3,9 @@ import os
 import sys
 import math
 
-##***********************##
-##        Conversion     ##
-##***********************##
+##*****************************##
+##        Conversion Start     ##
+##*****************************##
 
 print("write one of these [sc_hd, sc_hdll, sc_hs, sc_ls or sc_ms] to compile other SC variants")
 
@@ -14,3 +14,8 @@ typ = input("\n")#same as in script initial delay in SAs
 lib="sky130_fd_"+typ
 
 os.system("sh conversion_run.sh"+" "+str(lib))
+
+
+##***************************##
+##        Conversion End     ##
+##***************************##
